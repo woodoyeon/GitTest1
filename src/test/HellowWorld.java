5ec382ac52065b0d1893456a0ecad617a54a0a48
woodoyeon <dydy1212qwqw@naver.com>
2024-05-10 15:13:08 +0900

    test

--- src/test/HellowWorld.java
+++ src/test/HellowWorld.java
@@ -7,11 +7,13 @@ public class HellowWorld {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		String st = sc.next();
-		int n = sc.nextInt() - 1;
-
-		System.out.println(st.charAt(n));
-
-		sc.close();
+		System.out.println("Hello");
+		
+//		String st = sc.next();
+//		int n = sc.nextInt() - 1;
+//
+//		System.out.println(st.charAt(n));
+//
+//		sc.close();
 	}
 }
