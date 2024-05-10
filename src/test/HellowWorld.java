73c37dfb225916d164cd8b626c7380024b38e88b
woodoyeon <dydy1212qwqw@naver.com>
2024-05-10 15:11:21 +0900

    First Version

---
+++ src/test/HellowWorld.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String st = sc.next();
+		int n = sc.nextInt() - 1;
+
+		System.out.println(st.charAt(n));
+
+		sc.close();
+	}
+}
